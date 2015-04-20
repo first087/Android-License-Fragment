d3c396902b2a104f5f557180a469617f3da19ea0
Artit Kiuwilai <first087@gmail.com>
2015-04-20 13:39:14 +0700

    Initial

---
+++ license-fragment/src/main/java/com/ethanf/licensefragment/LicenseFragmentBase.java
@@ -0,0 +1,30 @@
+package com.ethanf.licensefragment;
+
+import android.os.Bundle;
+
+import com.inthecheesefactory.thecheeselibrary.fragment.support.v4.app.StatedFragment;
+
+public abstract class LicenseFragmentBase extends StatedFragment {
+
+    @Override
+    protected void onFirstTimeLaunched() {
+        super.onFirstTimeLaunched();
+
+        // TODO : Initial one time on create fragment
+    }
+
+    @Override
+    protected void onSaveState(Bundle outState) {
+        super.onSaveState(outState);
+
+        // TODO : Save State
+    }
+
+    @Override
+    protected void onRestoreState(Bundle savedInstanceState) {
+        super.onRestoreState(savedInstanceState);
+
+        // TODO : Restore State
+    }
+
+}
