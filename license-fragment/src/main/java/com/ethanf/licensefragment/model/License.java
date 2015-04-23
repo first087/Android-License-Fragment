44c540d0aeb89cf9a13d0ce51f0915261e5dd293
Artit Kiuwilai <first087@gmail.com>
2015-04-23 10:03:59 +0700

    Update generate license

--- license-fragment/src/main/java/com/ethanf/licensefragment/model/License.java
+++ license-fragment/src/main/java/com/ethanf/licensefragment/model/License.java
@@ -13,25 +13,37 @@ import java.io.InputStream;
  */
 public class License {
 
+    // Ref. http://opensource.org/licenses
+    public enum LicenseType {
+        APACHE_LICENSE_20,
+        BSD_3_CLAUSE,
+        BSD_2_CLAUSE,
+        GPL_30,
+        MIT_LICENSE
+        // TODO : Add new license type here
+    }
+
     private Context context;
     private String title;
-    private int rawId;
+    private LicenseType licenseType;
+    private String year;
+    private String owner;
 
     public License(Context context, int licenseId) {
         this.context = context;
 
         switch (licenseId) {
             /* ----- This Library ----- */
-            case LicenseID.LICENSE_FRAGMENT:    title = "License Fragment"; rawId = R.raw.licensefragment;  break;
+            case LicenseID.LICENSE_FRAGMENT:    title = "License Fragment"; licenseType = LicenseType.APACHE_LICENSE_20;    year = "2015";  owner = "Artit Kiuwilai";       break;
             /* ----- For This Library ----- */
-            case LicenseID.STATED_FRAGMENT:     title = "StatedFragment";   rawId = R.raw.statedfragment;   break;
+            case LicenseID.STATED_FRAGMENT:     title = "StatedFragment";   licenseType = LicenseType.APACHE_LICENSE_20;    year = "2015";  owner = "Sittiphol Phanvilai";  break;
             /* ----- Google Library ----- */
-            case LicenseID.GSON:                title = "Gson";             rawId = R.raw.gson;             break;
+            case LicenseID.GSON:                title = "Gson";             licenseType = LicenseType.APACHE_LICENSE_20;    year = "2008";  owner = "Google Inc.";          break;
             /* ----- Square Library ----- */
-            case LicenseID.OTTO:                title = "Otto";             rawId = R.raw.otto;             break;
-            case LicenseID.OKHTTP:              title = "OkHttp";           rawId = R.raw.okhttp;           break;
-            case LicenseID.RETROFIT:            title = "Retrofit";         rawId = R.raw.retrofit;         break;
-            case LicenseID.PICASSO:             title = "Picasso";          rawId = R.raw.picasso;          break;
+            case LicenseID.OTTO:                title = "Otto";             licenseType = LicenseType.APACHE_LICENSE_20;    year = "2013";  owner = "Square, Inc.";         break;
+            case LicenseID.OKHTTP:              title = "OkHttp";           licenseType = LicenseType.APACHE_LICENSE_20;    year = "2014";  owner = "Square, Inc.";         break;
+            case LicenseID.RETROFIT:            title = "Retrofit";         licenseType = LicenseType.APACHE_LICENSE_20;    year = "2013";  owner = "Square, Inc.";         break;
+            case LicenseID.PICASSO:             title = "Picasso";          licenseType = LicenseType.APACHE_LICENSE_20;    year = "2013";  owner = "Square, Inc.";         break;
             // TODO : Add new license case here
             default:                            throw new IllegalArgumentException();
         }
@@ -42,7 +54,19 @@ public class License {
     }
 
     public String getLicense() {
-        return readRawFile(rawId).trim();
+        return String.format(readRawFile(getRawId()).trim(), year, owner);
+    }
+
+    private int getRawId() {
+        switch (licenseType) {
+            case APACHE_LICENSE_20:     return R.raw.apache_license_v20;
+            case BSD_3_CLAUSE:          return R.raw.bsd_3_clause;
+            case BSD_2_CLAUSE:          return R.raw.bsd_2_clause;
+            case GPL_30:                return R.raw.gpl_30;
+            case MIT_LICENSE:           return R.raw.mit_license;
+            // TODO : Add new license type here
+            default:            throw new IllegalArgumentException();
+        }
     }
 
     private String readRawFile(int fileId) {
